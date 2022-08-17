4d53823e4cda4166ca044b761ef6a7c0090c5511
smlmbrt <sam.a.lambert@gmail.com>
2022-08-17 17:34:01 +0100

    Backfill REF/ALT alleles in matches, and reorder columns for efficient pl.concat without diagonal method

--- pgscatalog_utils/match/match.py
+++ pgscatalog_utils/match/match.py
@@ -13,22 +13,25 @@ def get_all_matches(scorefile: pl.DataFrame, target: pl.DataFrame, remove_ambigu
     scorefile_no_oa = scorefile_cat.filter(pl.col("other_allele") == None)
 
     matches: list[pl.DataFrame] = []
+    col_order = ['chr_name', 'chr_position', 'effect_allele', 'other_allele', 'effect_weight', 'effect_type', 'accession', 'effect_allele_FLIP', 'other_allele_FLIP',
+                 'ID',  'REF', 'ALT', 'is_multiallelic', 'matched_effect_allele', 'match_type']
+
 
     if scorefile_oa:
         logger.debug("Getting matches for scores with effect allele and other allele")
-        matches.append(_match_variants(scorefile_cat, target_cat, match_type="refalt"))
-        matches.append(_match_variants(scorefile_cat, target_cat, match_type="altref"))
+        matches.append(_match_variants(scorefile_cat, target_cat, match_type="refalt")[col_order])
+        matches.append(_match_variants(scorefile_cat, target_cat, match_type="altref")[col_order])
         if skip_flip is False:
-            matches.append(_match_variants(scorefile_cat, target_cat, match_type="refalt_flip"))
-            matches.append(_match_variants(scorefile_cat, target_cat, match_type="altref_flip"))
+            matches.append(_match_variants(scorefile_cat, target_cat, match_type="refalt_flip")[col_order])
+            matches.append(_match_variants(scorefile_cat, target_cat, match_type="altref_flip")[col_order])
 
     if scorefile_no_oa:
         logger.debug("Getting matches for scores with effect allele only")
-        matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_ref"))
-        matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_alt"))
+        matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_ref")[col_order])
+        matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_alt")[col_order])
         if skip_flip is False:
-            matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_ref_flip"))
-            matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_alt_flip"))
+            matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_ref_flip")[col_order])
+            matches.append(_match_variants(scorefile_no_oa, target_cat, match_type="no_oa_alt_flip")[col_order])
 
     return pl.concat(matches).pipe(postprocess_matches, remove_ambiguous)
 
@@ -100,12 +103,26 @@ def _match_variants(scorefile: pl.DataFrame, target: pl.DataFrame, match_type: s
             logger.critical(f"Invalid match strategy: {match_type}")
             raise Exception
 
+    # first_join = scorefile.join(target, score_keys, target_keys, how='inner')\
+    #     .with_columns([pl.col("*"),
+    #                    pl.col(effect_allele_column).alias("matched_effect_allele"),
+    #                    pl.lit(match_type).alias("match_type")]
+    #                   )
+    #
+    # print(match_type, first_join.columns)
+
+    missing_cols = ['REF', 'ALT']
+    if match_type.startswith('no_oa'):
+        if match_type.startswith('no_oa_ref'):
+            missing_cols = ['REF']
+        else:
+            missing_cols = ['ALT']
+    join_cols = ['ID'] + missing_cols
     return (scorefile.join(target, score_keys, target_keys, how='inner')
-            .with_columns([pl.col("*"),
-                           pl.col(effect_allele_column).alias("matched_effect_allele"),
-                           pl.lit(match_type).alias("match_type")])
-            .join(target, on="ID", how="inner")  # get REF / ALT back after first join
-            .drop(["#CHROM", "POS", "is_multiallelic_right"]))
+                    .with_columns([pl.col("*"),
+                                   pl.col(effect_allele_column).alias("matched_effect_allele"),
+                                   pl.lit(match_type).alias("match_type")])
+                    .join(target[join_cols], on="ID", how="inner"))  # get REF / ALT back after first join
 
 
 def _cast_categorical(scorefile, target) -> tuple[pl.DataFrame, pl.DataFrame]:

01e07188d05971105765c5615b43a316deabd98f
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-10-21 17:46:04 +0100

    lazy write

--- pgscatalog_utils/match/write.py
+++ pgscatalog_utils/match/write.py
@@ -35,34 +35,44 @@ def write_out(df: pl.LazyFrame, split: bool, outdir: str, dataset: str) -> None:
     if not os.path.isdir(outdir):
         os.mkdir(outdir)
 
-    logger.debug("Splitting by effect type")
-    effect_types: dict[str, pl.DataFrame] = _split_effect_type(df.collect())
+    for effect_type in ['additive', 'dominant', 'recessive']:
+        logger.debug(f"Splitting by effect type {effect_type}")
+        for i, x in enumerate(_deduplicate_variants(effect_type, df)):
+            effect_df: pl.LazyFrame = x.filter(pl.col('effect_type') == effect_type)
+            if effect_df.fetch().shape[0] > 0:
+                chroms: list[int] = effect_df.select("chr_name").unique().collect().get_column("chr_name").to_list()
+                params = {'chroms': chroms, 'effect_type': effect_type, 'i': str(i)}
+                _write_scorefile(params=params, scorefiles=effect_df, split=split, outdir=outdir, dataset=dataset)
+            else:
+                logger.debug(f"{effect_type} empty, skipping writing out")
+                continue
 
-    logger.debug("Deduplicating variants")
-    deduplicated: dict[str, pl.DataFrame] = {k: _deduplicate_variants(k, v) for k, v in effect_types.items()}
+    logger.debug("All scorefiles written, goodbye!")
 
-    logger.debug("Writing out scorefiles")
-    ea_dict: dict[str, str] = {'is_dominant': 'dominant', 'is_recessive': 'recessive', 'additive': 'additive'}
-    [_write_scorefile(ea_dict.get(k), v, split, outdir, dataset) for k, v in deduplicated.items()]
-
-
-def _write_scorefile(effect_type: str, scorefiles: pl.DataFrame, split: bool, outdir: str, dataset: str) -> None:
+def _write_scorefile(params: dict, scorefiles: pl.LazyFrame, split: bool, outdir: str, dataset: str) -> None:
     """ Write a list of scorefiles with the same effect type """
-    # each list element contains a dataframe of variants
-    # lists are split to ensure variants have unique ID - effect alleles
-    for i, scorefile in enumerate(scorefiles):
-        df_dict: dict[str, pl.DataFrame] = _format_scorefile(scorefile, split)  # may be split by chrom
+    effect_type = params.get('effect_type')
+    i = params.get('i')
+    chroms = params.get('chroms')
 
-        for k, v in df_dict.items():
-            chr = k.replace("false", "ALL")
-            path: str = os.path.join(outdir, f"{dataset}_{chr}_{effect_type}_{i}.scorefile.gz")
-            logger.debug(f"Writing matched scorefile to {path}")
+    dfs: list[pl.LazyFrame] = _format_scorefile(scorefiles, chroms)
 
-            with gzip.open(path, 'wb') as f:
-                v.write_csv(f, sep="\t")
+    if not split:
+        logger.debug("Writing combined scorefile")
+        chroms: list[str] = ["ALL"]  # reset chroms list and merge into one df
+        out_dfs: list[pl.LazyFrame] = [pl.concat(dfs)]
+    else:
+        out_dfs: list[pl.LazyFrame] = dfs
+        logger.debug("Writing split scorefiles")
+
+    for chrom, scorefile in zip(chroms, out_dfs):
+        fout: str = os.path.join(outdir, f"{dataset}_{chrom}_{effect_type}_{i}.scorefile.gz")
+        logger.debug(f"Writing matched scorefile to {fout}")
+        with gzip.open(fout, 'wb') as f:
+            scorefile.collect().write_csv(f, sep="\t")
 
 
-def _format_scorefile(df: pl.DataFrame, split: bool) -> dict[str, pl.DataFrame]:
+def _format_scorefile(df: pl.LazyFrame, chroms: list[str]) -> list[pl.LazyFrame]:
     """ Format a dataframe to plink2 --score standard
     Minimum example:
     ID | effect_allele | effect_weight
@@ -70,30 +80,17 @@ def _format_scorefile(df: pl.DataFrame, split: bool) -> dict[str, pl.DataFrame]:
     ID | effect_allele | weight_1 | ... | weight_n
     """
     logger.debug("Formatting scorefile to plink2 standard")
-    if split:
-        logger.debug("Split output requested")
-        chroms: list[int] = df["chr_name"].unique().to_list()
-        return {x: (df.filter(pl.col("chr_name") == x)
-                    .pivot(index=["ID", "matched_effect_allele"], values="effect_weight", columns="accession")
-                    .rename({"matched_effect_allele": "effect_allele"})
-                    .fill_null(strategy="zero"))
-                for x in chroms}
-    else:
-        logger.debug("Split output not requested")
-        formatted: pl.DataFrame = (
-            df.pivot(index=["ID", "matched_effect_allele"], values="effect_weight", columns="accession")
-            .rename({"matched_effect_allele": "effect_allele"})
-            .fill_null(strategy="zero"))
-        return {'false': formatted}
+    dfs = []
+    for chrom in chroms:
+        dfs.append(df.filter(pl.col("chr_name") == chrom).collect()
+                   .pivot(index=["ID", "matched_effect_allele"], values="effect_weight", columns="accession")
+                   .rename({"matched_effect_allele": "effect_allele"})
+                   .fill_null(strategy="zero")
+                   .lazy())
+    return dfs
 
 
-def _split_effect_type(df: pl.DataFrame) -> dict[str, pl.DataFrame]:
-    logger.debug("Splitting matches by effect type")
-    effect_types: list[str] = df["effect_type"].unique().to_list()
-    return {x: df.filter(pl.col("effect_type") == x) for x in effect_types}
-
-
-def _deduplicate_variants(effect_type: str, df: pl.DataFrame) -> list[pl.DataFrame]:
+def _deduplicate_variants(effect_type: str, df: pl.LazyFrame) -> list[pl.LazyFrame]:
     """ Find variant matches that have duplicate identifiers
     When merging a lot of scoring files, sometimes a variant might be duplicated
     this can happen when the matched effect allele differs at the same position, e.g.:
@@ -111,30 +108,26 @@ def _deduplicate_variants(effect_type: str, df: pl.DataFrame) -> list[pl.DataFra
     #   handled by pivoting, and it's pointless to split them unnecessarily
     # 2. use cumcount to number duplicate IDs
     # 3. join cumcount data on original DF, use this data for splitting
-    ea_count: pl.DataFrame = (df.select(["ID", "matched_effect_allele"])
+    ea_count: pl.LazyFrame = (df.select(["ID", "matched_effect_allele"])
     .unique()
     .with_columns([
         pl.col("ID").cumcount().over(["ID"]).alias("cumcount"),
         pl.col("ID").count().over(["ID"]).alias("count")
     ]))
 
-    dup_label: pl.DataFrame = df.join(ea_count, on=["ID", "matched_effect_allele"], how="left")
+    dup_label: pl.LazyFrame = df.join(ea_count, on=["ID", "matched_effect_allele"], how="left")
 
     # now split the matched variants, and make sure we don't lose any
-    n_splits: int = ea_count.select("cumcount").max()[0, 0] + 1  # cumcount = ngroup-1
+    n_splits: int = ea_count.select("cumcount").max().collect()[0, 0] + 1  # cumcount = ngroup-1
     df_lst: list = []
-    n_var: int = 0
 
     for i in range(0, n_splits):
-        x: pl.DataFrame = dup_label.filter(pl.col("cumcount") == i)
-        n_var += x.shape[0]
+        x: pl.LazyFrame = dup_label.filter(pl.col("cumcount") == i)
         df_lst.append(x)
 
     if len(df_lst) > 1:
         logger.debug(f"Duplicate variant identifiers split for effect type {effect_type}")
     else:
         logger.debug(f"No duplicate variant identifiers found for effect type {effect_type}")
-
-    assert n_var == df.shape[0]
 
     return df_lst

2c7216e53ff41334c669f5fa63cf180e1dfbbf27
smlmbrt <sam.a.lambert@gmail.com>
2022-10-21 13:39:30 +0100

    Handle descendant EFO terms to be excluded from trait queries

--- pgscatalog_utils/download/trait.py
+++ pgscatalog_utils/download/trait.py
@@ -6,16 +6,19 @@ from pgscatalog_utils.download.score import query_api
 logger = logging.getLogger(__name__)
 
 
-def query_trait(trait: str, user_agent:str = None) -> list[str]:
+def query_trait(trait: str, user_agent:str = None, include_children:bool = True) -> list[str]:
     logger.debug(f"Querying PGS Catalog with trait {trait}")
-    api: str = f'/trait/{trait}?include_children=1'
+    api: str = f'/trait/{trait}?include_children=0'
     results_json = query_api(api, user_agent)
 
     if results_json == {} or results_json == None:
         logger.critical(f"Bad response from PGS Catalog for EFO term: {trait}")
         raise Exception
 
-    keys: list[str] = ['associated_pgs_ids', 'child_associated_pgs_ids']
+    keys: list[str] = ['associated_pgs_ids']
+    if include_children:
+        keys.append('child_associated_pgs_ids')
+
     pgs: list[str] = []
     for key in keys:
         pgs.append(results_json.get(key))

5a202b5157731c74a8426a63af6894d7da6bdf66
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2022-10-21 18:56:38 +0100

    fix test with extreme prejudice

--- tests/match/test_match.py
+++ tests/match/test_match.py
@@ -10,20 +10,6 @@ from pgscatalog_utils.match.match import get_all_matches
 from pgscatalog_utils.match.match_variants import match_variants
 
 
-def test_match_pass(mini_scorefile, target_path, tmp_path):
-    out_dir = str(tmp_path.resolve())
-
-    args: list[str] = ['match_variants', '-s', mini_scorefile,
-                       '-t', target_path,
-                       '-d', 'test',
-                       '--min_overlap', 0,
-                       '--outdir', out_dir,
-                       '--keep_ambiguous', '--keep_multiallelic']
-
-    with patch('sys.argv', args):
-        match_variants()
-
-
 def test_match_fail(mini_scorefile, target_path, tmp_path):
     out_dir = str(tmp_path.resolve())
 

86151ce242569c91d382944238c3000347a57f9b
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-02-15 14:13:12 +0000

    add split mode

--- pgscatalog_utils/relabel/relabel_ids.py
+++ pgscatalog_utils/relabel/relabel_ids.py
@@ -18,6 +18,8 @@ def _parse_args(args=None):
     parser.add_argument("--col_to", help='column to change TO', dest='col_to', required=True)
     parser.add_argument("--target_file", help='target file', dest='target_file', required=True)
     parser.add_argument("--target_col", help='target column to revalue', dest='target_col', required=True)
+    parser.add_argument("--split", help='split output file', dest='split', action='store_true',
+                        required=False)
     parser.add_argument("--out", help='output filename', dest='out_file', required=True)
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
                         help='<Optional> Extra logging information')
@@ -54,25 +56,49 @@ def open_map(path, col_from, col_to):
             return _read_map(in_map, col_from, col_to)
 
 
+def _open_output(path, header):
+    logger.debug(f"Opening {path} and writing header")
+    outf = open(path, 'w')
+    outf.write('\t'.join(header) + '\n')
+    return outf
+
+
 def relabel_ids():
     args = _parse_args()
     config.set_logging_level(args.verbose)
 
     map_list = [open_map(x, args.col_from, args.col_to) for x in args.map_files]
     mapping = reduce(operator.ior, map_list, {})  # merge dicts quickly, ior is equivalent to | operator
+    del map_list
 
     # Read, relabel and output file
-    with open(args.out_file, 'w') as outf:
-        with open(args.target_file, 'r') as in_target:
-            h = in_target.readline().strip().split()
-            i_target_col = h.index(args.target_col)
-            outf.write('\t'.join(h) + '\n')
+    with open(args.target_file, 'r') as in_target:
+        h = in_target.readline().strip().split()
+        i_target_col = h.index(args.target_col)
 
-            # relabel lines
-            for line in in_target:
-                line = line.strip().split()
-                line[i_target_col] = mapping[line[i_target_col]]  # revalue column
-                outf.write('\t'.join(line) + '\n')
+        if not args.split:
+            current_chrom: str = 'ALL'
+            outf = _open_output(f"{current_chrom}_{args.out_file}", h)
+
+        for i, line in enumerate(in_target):
+            line = line.strip().split()
+
+            if args.split and i == 0:
+                current_chrom = line[0]
+                logger.debug(f"Creating split output, current chrom: {line[0]}")
+                outf = _open_output(f"{current_chrom}_{args.out_file}", h)
+
+            if args.split and current_chrom != line[0]:
+                logger.debug(f"New chromosome {line[0]} detected in split mode, writing to new file")
+                outf.close()
+                current_chrom = line[0]
+                outf = _open_output(f"{current_chrom}_{args.out_file}", h)
+
+            line[i_target_col] = mapping[line[i_target_col]]  # revalue column
+            outf.write('\t'.join(line) + '\n')
+
+        outf.close()
+        logger.debug("Finished relabelling")
 
 
 if __name__ == "__main__":

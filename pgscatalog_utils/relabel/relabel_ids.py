08443685fd4754fd813ef4d32b1bdb02258bf0f5
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-02-15 10:32:15 +0000

    add support for gzipped input mapping files

--- pgscatalog_utils/relabel/relabel_ids.py
+++ pgscatalog_utils/relabel/relabel_ids.py
@@ -1,6 +1,12 @@
 import argparse
+import gzip
+import logging
 from functools import reduce
 import operator
+
+from pgscatalog_utils import config
+
+logger = logging.getLogger(__name__)
 
 
 def _parse_args(args=None):
@@ -13,28 +19,46 @@ def _parse_args(args=None):
     parser.add_argument("--target_file", help='target file', dest='target_file', required=True)
     parser.add_argument("--target_col", help='target column to revalue', dest='target_col', required=True)
     parser.add_argument("--out", help='output filename', dest='out_file', required=True)
-
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='<Optional> Extra logging information')
     return parser.parse_args(args)
 
 
-def read_map(path, col_from, col_to):
-    # Read the mapping file
-    with open(path, 'r') as in_map:
-        h = in_map.readline().strip().split()
-        i_from = h.index(col_from)
-        i_to = h.index(col_to)
+def _is_gz_file(filepath):
+    with open(filepath, 'rb') as test_f:
+        return test_f.read(2) == b'\x1f\x8b'
 
-        mapping = {}
-        for line in in_map:
-            line = line.strip().split()
-            mapping[line[i_from]] = line[i_to]
+
+def _read_map(in_map, col_from, col_to):
+    h = in_map.readline().strip().split()
+    i_from = h.index(col_from)
+    i_to = h.index(col_to)
+
+    mapping = {}
+    for line in in_map:
+        line = line.strip().split()
+        mapping[line[i_from]] = line[i_to]
+
     return mapping
+
+
+def open_map(path, col_from, col_to):
+    # Read the mapping file
+    if _is_gz_file(path):
+        logger.debug(f"Reading map file {path} with gzip.open")
+        with gzip.open(path, 'rt') as in_map:
+            return _read_map(in_map, col_from, col_to)
+    else:
+        logger.debug(f"Reading map file {path} with open")
+        with open(path, 'r') as in_map:
+            return _read_map(in_map, col_from, col_to)
 
 
 def relabel_ids():
     args = _parse_args()
+    config.set_logging_level(args.verbose)
 
-    map_list = [read_map(x, args.col_from, args.col_to) for x in args.map_files]
+    map_list = [open_map(x, args.col_from, args.col_to) for x in args.map_files]
     mapping = reduce(operator.ior, map_list, {})  # merge dicts quickly, ior is equivalent to | operator
 
     # Read, relabel and output file

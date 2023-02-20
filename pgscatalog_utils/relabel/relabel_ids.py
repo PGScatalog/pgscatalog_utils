8efdf8ae06785da436906c711f16589f49469e8c
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-02-20 17:46:10 +0000

    improve --combined functionality and tidy output path

--- pgscatalog_utils/relabel/relabel_ids.py
+++ pgscatalog_utils/relabel/relabel_ids.py
@@ -13,16 +13,23 @@ def _parse_args(args=None):
     parser = argparse.ArgumentParser(
         description="Relabel the column values in one file based on a pair of columns in another",
         formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-d', '--dataset', dest='dataset', required=True,
+                        help='<Required> Label for target genomic dataset')
     parser.add_argument("-m", "--maps", help='mapping filenames', dest='map_files', nargs='+', required=True)
     parser.add_argument("--col_from", help='column to change FROM', dest='col_from', required=True)
     parser.add_argument("--col_to", help='column to change TO', dest='col_to', required=True)
     parser.add_argument("--target_file", help='target file', dest='target_file', required=True)
     parser.add_argument("--target_col", help='target column to revalue', dest='target_col', required=True)
-    parser.add_argument("--out", help='output filename', dest='out_file', required=True)
     parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
                         help='<Optional> Extra logging information')
     parser.add_argument('--split', dest='split', action='store_true', required=False)
-    return parser.parse_args(args)
+    parser.add_argument('--combined', dest='combined', action='store_true', required=False)
+    args = parser.parse_args()
+
+    if not (args.split or args.combined):
+        parser.error("At least one of --combined or --split is required")
+
+    return args
 
 
 def _is_gz_file(filepath):
@@ -75,32 +82,20 @@ def _get_chrom(line, id_idx):
     return line[id_idx].split(':')[0]
 
 
-def relabel_ids():
-    args = _parse_args()
-    config.set_logging_level(args.verbose)
+def _get_outf_path(current_chrom, dataset):
+    return f"{dataset}_{current_chrom}_relabelled.gz"
 
-    # more than one mapping file input -> assume split input, so produce split output
-    if len(args.map_files) > 1 or args.split:
-        logger.debug(f"--maps n args {len(args.map_files)}, setting split_output = True")
-        split_output = True
-    else:
-        logger.debug(f"--maps n args {len(args.map_files)}, setting split_output = False")
-        split_output = False
 
-    map_list = [open_map(x, args.col_from, args.col_to) for x in args.map_files]
-
-    mapping = reduce(operator.ior, map_list, {})  # merge dicts quickly, ior is equivalent to | operator
-    del map_list
-
-    # Read, relabel and output file
+def _relabel(args, mapping, split_output):
     with _open_target(args.target_file) as in_target:
         h = in_target.readline().strip().split()
         i_target_col = h.index(args.target_col)
-        out_suffix = ".gz"
+
 
         if not split_output:
             current_chrom: str = 'ALL'
-            outf = _open_output(f"{current_chrom}_{args.out_file}{out_suffix}", h)
+            outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
+            outf = _open_output(outf_path, h)
 
         for i, line in enumerate(in_target):
             line = line.strip().split()
@@ -112,20 +107,45 @@ def relabel_ids():
             if split_output and i == 0:
                 current_chrom = _get_chrom(line, id_idx)
                 logger.debug(f"Creating split output, current chrom: {_get_chrom(line, id_idx)}")
-                outf = _open_output(f"{current_chrom}_{args.out_file}{out_suffix}", h)
+                outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
+                outf = _open_output(outf_path, h)
 
             if split_output and current_chrom != _get_chrom(line, id_idx):
                 logger.debug(f"New chromosome {_get_chrom(line, id_idx)} detected in split mode, writing to new file")
                 outf.close()
 
                 current_chrom = _get_chrom(line, id_idx)
-                outf = _open_output(f"{current_chrom}_{args.out_file}{out_suffix}", h)
+                outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
+                outf = _open_output(outf_path, h)
 
             line[i_target_col] = mapping[line[i_target_col]]  # revalue column
             outf.write('\t'.join(line) + '\n')
 
         outf.close()
-        logger.debug("Finished relabelling")
+
+
+def relabel_ids():
+    args = _parse_args()
+    config.set_logging_level(args.verbose)
+
+    # sometimes we want to write the relabelled data out as split _and_ combined
+    split_output = []
+    if args.split:
+        logger.debug("Writing split output enabled")
+        split_output.append(True)
+
+    if args.combined:
+        logger.debug("Writing combined output enabled")
+        split_output.append(False)
+
+    map_list = [open_map(x, args.col_from, args.col_to) for x in args.map_files]
+    mapping = reduce(operator.ior, map_list, {})  # merge dicts quickly, ior is equivalent to | operator
+    del map_list
+
+    # Read, relabel and output file
+    # note: if --split and --combined, reads the input file twice. not ideal but it's pretty quick
+    [_relabel(args=args, mapping=mapping, split_output=x) for x in split_output]
+    logger.debug("Finished relabelling")
 
 
 if __name__ == "__main__":

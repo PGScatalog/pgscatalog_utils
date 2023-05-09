f15969e9032e8be6891055f1fe4b196029c44feb
Benjamin Wingfield <bwingfield@ebi.ac.uk>
2023-05-09 14:05:52 +0100

    add zstd support

--- pgscatalog_utils/relabel/relabel_ids.py
+++ pgscatalog_utils/relabel/relabel_ids.py
@@ -1,8 +1,11 @@
 import argparse
 import gzip
+import io
 import logging
 import operator
 from functools import reduce
+
+import zstandard
 
 from pgscatalog_utils import config
 
@@ -38,6 +41,11 @@ def _is_gz_file(filepath):
         return test_f.read(2) == b'\x1f\x8b'
 
 
+def _is_zstd_file(filepath):
+    with open(filepath, 'rb') as test_f:
+        return test_f.read(4) == b'\x28\xb5\x2f\xfd'
+
+
 def _read_map(in_map, col_from, col_to):
     h = in_map.readline().strip().split()
     i_from = h.index(col_from)
@@ -63,13 +71,13 @@ def open_map(path, col_from, col_to):
             return _read_map(in_map, col_from, col_to)
 
 
-def _open_target(path):
+def _detect_target(path):
     if _is_gz_file(path):
-        logger.debug("Opening target with gzip.open")
-        return gzip.open(path, 'rt')
+        return "gzip"
+    elif _is_zstd_file(path):
+        return "zstd"
     else:
-        logger.debug("Opening target with open")
-        return open(path, 'r')
+        return "text"
 
 
 def _open_output(path, header):
@@ -87,47 +95,61 @@ def _get_outf_path(current_chrom, dataset):
     return f"{dataset}_{current_chrom}_relabelled.gz"
 
 
-def _relabel(args, mapping, split_output):
-    with _open_target(args.target_file) as in_target:
-        h = in_target.readline()
-        if args.comment_char:
-            while h.startswith(args.comment_char):
-                h = in_target.readline()
-        h = h.strip().split()
+def _relabel_target(args, mapping, split_output):
+    with open(args.target_file, 'rb') as f:
+        match _detect_target(args.target_file):
+            case 'zstd':
+                dctx = zstandard.ZstdDecompressor()
+                with dctx.stream_reader(f) as reader:
+                    _relabel(in_target=io.TextIOWrapper(reader), mapping=mapping, split_output=split_output, args=args)
+            case 'gzip':
+                with gzip.open(f) as reader:
+                    _relabel(in_target=reader, mapping=mapping, split_output=split_output, args=args)
+            case 'text':
+                _relabel(in_target=f, mapping=mapping, split_output=split_output, args=args)
+            case _:
+                raise Exception("Can't detect target format")
 
 
-        i_target_col = h.index(args.target_col)
+def _relabel(in_target, args, mapping, split_output):
+    h = in_target.readline()
+    if args.comment_char:
+        while h.startswith(args.comment_char):
+            h = in_target.readline()
+    h = h.strip().split()
 
-        if not split_output:
-            current_chrom: str = 'ALL'
+    i_target_col = h.index(args.target_col)
+
+    if not split_output:
+        current_chrom: str = 'ALL'
+        outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
+        outf = _open_output(outf_path, h)
+
+    for i, line in enumerate(in_target):
+        line = line.strip().split()
+        # get the first column index that contains :
+        # assume this contains a variant ID e.g. 1:1234:A:C
+        # this column index can change across different types of files
+        id_idx = [i for i, x in enumerate(line) if ':' in x][0]
+
+        if split_output and i == 0:
+            current_chrom = _get_chrom(line, id_idx)
+            logger.debug(f"Creating split output, current chrom: {_get_chrom(line, id_idx)}")
             outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
             outf = _open_output(outf_path, h)
 
-        for i, line in enumerate(in_target):
-            line = line.strip().split()
-            # get the first column index that contains :
-            # assume this contains a variant ID e.g. 1:1234:A:C
-            # this column index can change across different types of files
-            id_idx = [i for i, x in enumerate(line) if ':' in x][0]
+        if split_output and current_chrom != _get_chrom(line, id_idx):
+            logger.debug(f"New chromosome {_get_chrom(line, id_idx)} detected in split mode, writing to new file")
+            outf.close()
 
-            if split_output and i == 0:
-                current_chrom = _get_chrom(line, id_idx)
-                logger.debug(f"Creating split output, current chrom: {_get_chrom(line, id_idx)}")
-                outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
-                outf = _open_output(outf_path, h)
+            current_chrom = _get_chrom(line, id_idx)
+            outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
+            outf = _open_output(outf_path, h)
 
-            if split_output and current_chrom != _get_chrom(line, id_idx):
-                logger.debug(f"New chromosome {_get_chrom(line, id_idx)} detected in split mode, writing to new file")
-                outf.close()
+        line[i_target_col] = mapping[line[i_target_col]]  # revalue column
+        outf.write('\t'.join(line) + '\n')
 
-                current_chrom = _get_chrom(line, id_idx)
-                outf_path = _get_outf_path(current_chrom=current_chrom, dataset=args.dataset)
-                outf = _open_output(outf_path, h)
-
-            line[i_target_col] = mapping[line[i_target_col]]  # revalue column
-            outf.write('\t'.join(line) + '\n')
-
-        outf.close()
+    outf.close()
 
 
 def relabel_ids():
@@ -154,7 +176,7 @@ def relabel_ids():
 
     # Read, relabel and output file
     # note: if --split and --combined, reads the input file twice. not ideal but it's pretty quick
-    [_relabel(args=args, mapping=mapping, split_output=x) for x in split_output]
+    [_relabel_target(args=args, mapping=mapping, split_output=x) for x in split_output]
     logger.debug("Finished relabelling")
 
 
